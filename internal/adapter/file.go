a6cdde21eda6f4ac5aa19971fb46cfaf83e0f351
pimg <pim.gaemers@gmail.com>
2024-02-24 11:42:41 +0100

    save CRL files to local file cache

---
+++ internal/adapter/file.go
@@ -0,0 +1,46 @@
+package adapter
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type FileCache struct {
+	cacheDir string
+}
+
+func NewFileCache() error {
+	cacheDir, err := determineCacheDir()
+	if err != nil {
+		return err
+	}
+
+	err = os.MkdirAll(cacheDir, 0o777)
+	if err != nil {
+		return err
+	}
+
+	GlobalCache = &FileCache{cacheDir: cacheDir}
+
+	return nil
+}
+
+func (f *FileCache) Write(filename string, fileContent []byte) error {
+	err := os.WriteFile(f.cacheDir+filename, fileContent, 0o777)
+	if err != nil {
+		return errors.Join(err, fmt.Errorf("failed to write filename to cache: %s", filename))
+	}
+
+	return nil
+}
+
+func determineCacheDir() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", errors.New("could not create file path to User home dir, Cache will not be enabled")
+	}
+
+	return filepath.Join(homeDir, ".cache", "certguard"), nil
+}
