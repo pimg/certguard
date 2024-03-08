d25301357352dc53e857b8d76c949d0fa68a4b76
Pim Gaemers <pimg@users.noreply.github.com>
2024-03-08 18:58:18 +0100

    refactor to properly use adapters (#18)

--- internal/adapter/file.go
+++ internal/adapter/file.go
@@ -36,6 +36,10 @@ func (f *FileCache) Write(filename string, fileContent []byte) error {
 	return nil
 }
 
+func (f *FileCache) Read(filename string) ([]byte, error) {
+	return os.ReadFile(filename)
+}
+
 func (f *FileCache) Dir() string {
 	return f.cacheDir
 }
--- internal/ports/models/commands/crl_request.go
+++ internal/ports/models/commands/crl_request.go
@@ -6,18 +6,28 @@ import (
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/pimg/certguard/internal/adapter"
 	"github.com/pimg/certguard/internal/ports/models/messages"
 	"github.com/pimg/certguard/pkg/crl"
 )
 
 func GetCRL(requestURL string) tea.Cmd {
 	return func() tea.Msg {
-		revocationList, err := crl.FetchRevocationList(strings.TrimSpace(requestURL))
+		revocationListURL := strings.TrimSpace(requestURL)
+		revocationList, err := crl.FetchRevocationList(revocationListURL)
 		if err != nil {
 			return messages.ErrorMsg{
 				Err: errors.Join(fmt.Errorf("could not download CRL with provided URL: %s", requestURL), err),
 			}
 		}
+
+		filename := revocationListURL[strings.LastIndex(revocationListURL, "/"):]
+
+		err = adapter.GlobalCache.Write(filename, revocationList.Raw)
+		if err != nil {
+			return messages.ErrorMsg{Err: errors.Join(err, fmt.Errorf("cannot write the CRL to the cache: %s", filename))}
+		}
+
 		return messages.CRLResponseMsg{
 			RevocationList: revocationList,
 		}
@@ -26,10 +36,17 @@ func GetCRL(requestURL string) tea.Cmd {
 
 func LoadCRL(path string) tea.Cmd {
 	return func() tea.Msg {
-		revocationList, err := crl.LoadRevocationList(strings.TrimSpace(path))
+		rawCRL, err := adapter.GlobalCache.Read(path)
 		if err != nil {
 			return messages.ErrorMsg{
 				Err: errors.Join(fmt.Errorf("could not load CRL from cache location: %s", path), err),
+			}
+		}
+
+		revocationList, err := crl.ParseRevocationList(rawCRL)
+		if err != nil {
+			return messages.ErrorMsg{
+				Err: errors.Join(errors.New("could not parse CRL"), err),
 			}
 		}
 		return messages.CRLResponseMsg{
